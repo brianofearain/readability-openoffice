9ca4ace73c13a3f14d15ae3ef120129fa77c121e
brianferan <brianjferan@gmail.com>
2014-04-12 23:01:07 +0200

    Initial Commit

---
+++ com.am.bt.address/src/com/am/bt/address/Street.java
@@ -0,0 +1,59 @@
+package com.am.bt.address;
+
+public class Street {
+	public String houseNum;
+	public String streetPrefix;
+	public String streetName;
+	public String streetType;
+	public String streetSuffix;
+	public String apt;
+
+	public String getHouseNum() {
+		return houseNum;
+	}
+
+	public void setHouseNum(String houseNum) {
+		this.houseNum = houseNum;
+	}
+
+	public String getStreetPrefix() {
+		return streetPrefix;
+	}
+
+	public void setStreetPrefix(String streetPrefix) {
+		this.streetPrefix = streetPrefix;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getStreetType() {
+		return streetType;
+	}
+
+	public void setStreetType(String streetType) {
+		this.streetType = streetType;
+	}
+
+	public String getStreetSuffix() {
+		return streetSuffix;
+	}
+
+	public void setStreetSuffix(String streetSuffix) {
+		this.streetSuffix = streetSuffix;
+	}
+
+	public String getApt() {
+		return apt;
+	}
+
+	public void setApt(String apt) {
+		this.apt = apt;
+	}
+
+}
