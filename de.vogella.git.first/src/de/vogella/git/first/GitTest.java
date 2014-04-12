8f3ca8b90f2b76c6937e839175fb1c328d3b8e39
brianferan <brianjferan@gmail.com>
2014-04-12 15:39:32 +0200

    commit 2

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -2,6 +2,6 @@ package de.vogella.git.first;
 
 public class GitTest {
   public static void main(String[] args) {
-    System.out.println("Git is fun");
+    System.out.println("Git is not fun");
   }
 }
